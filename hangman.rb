708f7fa0cb25162ec59ae8c5a8d60de0399f1d61
jwiddison <jordan.widdison@gmail.com>
2019-02-05 22:49:20 -0700

    Handle bad inputs in hangman

--- hangman.rb
+++ hangman.rb
@@ -1,45 +1,70 @@
 #!/usr/bin/env ruby
 
 def play
-  puts "\n"
-  puts '##################'
-  puts '#### HANG MAN ####'
-  puts '##################'
-  puts "\n"
-  puts "What word should we guess?"
-  word = gets.chomp.upcase
   system "clear" or system "cls"
-  prompt = word.split('').map { |x| "_ " }.join('')
+  puts "##################"
+  puts '#### HANG MAN ####'
+  puts "##################"
+
   wrong_letters = []
   correct_letters = []
 
+  puts "\nWhat word do you want people to guess?"
+  print '#: '
+  word = gets.chomp.upcase
+  prompt = word.split('').map { |x| "_ " }.join('')
+
   while wrong_letters.length < 6
-    puts "\n#{prompt}\n"
-    unless wrong_letters.empty?
-      puts 'Incorrect letters you\'ve guessed:'
-      puts wrong_letters.join(' ')
+    system "clear" or system "cls"
+
+    if !wrong_letters.empty?
+      puts "\nIncorrect letters you've guessed:"
+      puts wrong_letters.join(' ') + "\n"
     end
+
+    puts "\n#{prompt}\n"
 
     puts "\nGuess a letter:"
     print '#: '
     guess = gets.chomp
 
-    if word.include?(guess.upcase)
-      correct_letters << guess.upcase
-      prompt = word.split('').map do |letter|
-        correct_letters.include?(letter) ? "#{letter} " : "_ "
-      end.join('')
-    else
-      wrong_letters << guess.upcase
-      puts "Sorry, no #{guess.upcase}"
-    end
+    if guess.length == 1
+      if word.include?(guess.upcase)
+        correct_letters << guess.upcase
+        prompt = word.split('').map do |letter|
+          correct_letters.include?(letter) ? "#{letter} " : "_ "
+        end.join('')
+      else
+        if wrong_letters.include?(guess.upcase)
+          puts "\n\nYou've already guessed #{guess.upcase}!!"
+          sleep(1)
+        else
+          wrong_letters << guess.upcase
+          puts "Sorry, no #{guess.upcase}"
+        end
+      end
 
-    if prompt.gsub('_', '').gsub(' ', '') == word
-      puts prompt
-      puts 'YOU DID IT!!!'
-      break
+      if prompt.gsub('_', '').gsub(' ', '') == word
+        puts prompt
+        puts 'YOU DID IT!!!'
+        break
+      end
+    else
+      puts "\n\nOnly guess one letter at a time!!"
+      sleep(1)
     end
   end
+
+  # You didn't win
+  puts "\n\nBetter luck next time!!\n\n"
+  puts "   _________   "
+  puts "  |         |  "
+  puts "  |         0  "
+  puts "  |        /|\\"
+  puts "  |        / \\"
+  puts "  |            "
+  puts "  |            "
+  puts "------         "
 end
 
 play()
