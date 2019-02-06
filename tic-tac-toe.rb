0befe6ccc75822ad000c5aa1ef5103af473b7289
jwiddison <jordan.widdison@gmail.com>
2019-02-05 19:24:33 -0700

    Make the main method work, everything but picking the winner

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,34 +1,8 @@
-row1 = '[ ][ ][ ]'
-row2 = '[ ][ ][ ]'
-row3 = '[ ][ ][ ]'
-
-string_board = "#{row1}\n#{row2}\n#{row3}"
-
-puts "\n\n### TIC TAC TOE ###\n\n"
-puts string_board
-puts "\n>> Made for Young Men's <<\n\n"
-
-board = [
-  [[],[],[]],
-  [[],[],[]],
-  [[],[],[]]
-]
-
-turn = 1
-
-options = {
-  1 => '1: top-left',
-  2 => '2: top-center',
-  3 => '3: top-right',
-  4 => '4: middle-left',
-  5 => '5: middle-center',
-  6 => '6: middle-right',
-  7 => '7: bottom-left',
-  8 => '8: bottom-center',
-  9 => '9: bottom-right'
-}
+#!/usr/bin/env ruby
 
 =begin
+
+  A simple tic-tac-toe game played from the command line implemented in Ruby
 
   The numbered moves map to the board like this:
 
@@ -38,35 +12,56 @@ options = {
 
 =end
 
-while turn < 10 do
-  # puts "turn: #{turn}"
-  puts "#{turn.odd? ? 'X' : '0'}'s turn.  Enter the number of which move you want to make:"
-  options.each do |key, value|
-    puts "#{value}"
+def main
+  puts "\n\n### TIC TAC TOE ###\n\n"
+
+  board = [
+    [' ',' ',' '],
+    [' ',' ',' '],
+    [' ',' ',' ']
+  ]
+
+  print_board(board)
+
+  turn = 1
+
+  options = {
+    1 => '1: top-left',
+    2 => '2: top-center',
+    3 => '3: top-right',
+    4 => '4: middle-left',
+    5 => '5: middle-center',
+    6 => '6: middle-right',
+    7 => '7: bottom-left',
+    8 => '8: bottom-center',
+    9 => '9: bottom-right'
+  }
+
+  while turn < 10 do
+    player = turn.odd? ? 'x' : 'o'
+    puts "\n#{player.upcase}'s turn.  Enter the number for the move you want to make:"
+    options.each { |k, v| puts "#{v}" }
+    move = gets.chomp
+
+    row = (move.to_i - 1) / 3
+    column = (move.to_i - 1) % 3
+    board[row][column] = player
+
+    print_board(board)
+    options.delete(move.to_i)
+
+    check_for_winner(board)
+    turn += 1
   end
-  move = gets.chomp
-
-  # row =
-  #   case move
-  #   when include?('top')
-  #     0
-  #   when include?('middle')
-  #     1
-  #   when include?('bottom')
-  #     2
-  #   end
-  #
-  # column =
-  #   case move
-  #   when include?('left')
-  #     0
-  #   when include?('center')
-  #     1
-  #   when include?('bottom')
-  #     2
-  #   end
-
-  # puts "row: #{row} column: #{column}"
-
-  turn += 1
 end
+
+def print_board(board)
+  board.each do |row|
+    puts row.reduce("") { |string, column| string << "[#{column}]" }
+  end
+end
+
+def check_for_winner(board)
+end
+
+main()
